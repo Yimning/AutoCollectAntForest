f5ec4c8ddd9388b87013bea04082c205d1a6db0c
Yimning <1148967988@qq.com>
2021-01-23 19:18:44 +0800

    点亮锁屏的函数

--- FindCollectAntForest.js
+++ FindCollectAntForest.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Yimning
  * @Date: 2021-01-22 16:27:03
- * @LastEditTime: 2021-01-23 19:16:43
+ * @LastEditTime: 2021-01-23 19:18:29
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \undefinedc:\Users\Yimning\Desktop\脚本\-Auto.js-\FindCollectAntForest.js
@@ -95,7 +95,5 @@ function unlock() {
         //click(180,1700);//4
 
         // click(180,1800);//7
-
-
     }
 }
